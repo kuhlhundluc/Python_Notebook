0fea973ede037bf12cb7e1d1824eb962a6aa40a6
Stephen Brown <smbrown@localhost.localdomain>
2019-02-13 10:47:36 -0500

    Full new install of original Python Notebook for github.  Also will be linked to Google Drive. Everything will be subfoldered locally in git_dir/Python_Notebook Sb

---
+++ Python Notebook/Crash Course in Python/apostrophe.py
@@ -0,0 +1,4 @@
+#lets play with strings and apostrophe's
+message = 'One of Python's strengths is its diverse community.'
+print(message)
+
---
+++ Python Notebook/simple_message.py
@@ -0,0 +1,7 @@
+#simple_message.py
+
+message1="These are not the droids you are looking for."
+message2="These are not the droids we are looking for."
+
+print(message1)
+print(message2)
